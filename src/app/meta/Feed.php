687fd300de7ec6244790e9c5c50fd601b489c9c0
jibo <jibo@outlook.com>
2017-04-23 00:42:12 +0800

    Update product size option filter for feed

--- src/app/meta/Feed.php
+++ src/app/meta/Feed.php
@@ -63,13 +63,25 @@ class Feed extends AppBase
             $category = $this->db->exec('SELECT c.name FROM oc_category_description c, oc_product_to_category p WHERE p.product_id = ? AND p.category_id = c.category_id', $product['id']);
             $data[] = implode(';', array_column($category, 'name'));
 
-            $size = $this->db->exec(<<<SQL
-SELECT n.name FROM oc_product_option_value v, oc_option_description o, oc_option_value_description n WHERE v.product_id = ${product['id']} AND v.option_id = o.option_id AND o.name = 'size for shoes' AND v.option_value_id = n.option_value_id  ORDER by n.name
-SQL
-            );
-            $data[] = implode(';', array_column($size, 'name'));;
+            $size = $this->db->exec("SELECT n.name FROM oc_product_option_value v, oc_option_description o, oc_option_value_description n WHERE v.product_id = ${product['id']} AND v.option_id = o.option_id AND o.name LIKE 'size%' AND v.option_value_id = n.option_value_id  ORDER by n.name");
+            $data[] = implode(';', array_column($size, 'name'));
 
-            fputcsv($fp, $data);
+            $options = $this->getMultiOptionImages($product['id']);
+
+            if ($options) {
+                $head = array_flip(self::HEAD);
+                foreach ($options as $option) {
+                    $sub = $data;
+                    $sub[$head['id']] .= '-' . $option['sku'];
+                    $sub[$head['link']] .= '&poip_ov=' . $option['povId'];
+                    $sub[$head['model']] = $option['sku'];
+                    $sub[$head['image']] = self::PREFIX . $option['image'];
+                    fputcsv($fp, $sub);
+                }
+            } else {
+                fputcsv($fp, $data);
+            }
+
             ob_flush();
             flush();
         }
@@ -77,5 +89,26 @@ SQL
         fclose($fp);
 
         \Base::instance()->log($this->db->log());
+    }
+
+    function getMultiOptionImages($id)
+    {
+        $results = [];
+
+        $data = $this->db->exec("SELECT DISTINCT(product_option_value_id) FROM oc_poip_option_image WHERE product_id = $id");
+
+        if (count($data) > 1) {
+            foreach ($data as $item) {
+                $povId = $item['product_option_value_id'];
+                list($info) = $this->db->exec("SELECT sub_sku, image FROM oc_product_option_value p, oc_poip_option_image m WHERE p.product_option_value_id = $povId AND p.product_option_value_id = m.product_option_value_id ORDER BY sort_order LIMIT 1");
+                $results[] = [
+                    'povId' => $povId,
+                    'sku' => $info['sub_sku'],
+                    'image' => $info['image']
+                ];
+            }
+        }
+
+        return $results;
     }
 }
