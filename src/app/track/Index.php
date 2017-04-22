67716c7ceb11de33e5c4cc79d98b0553191de540
jibo <jibo@outlook.com>
2017-04-22 09:30:12 +0800

    Display track statistics

--- src/app/track/Index.php
+++ src/app/track/Index.php
@@ -4,27 +4,47 @@ namespace app\track;
 use app\common\AppBase;
 use DB\SQL\Mapper;
 
-class Index extends AppBase {
+class Index extends AppBase
+{
     private $page = [
-        'no' => 1,
-        'size' => 25,
+        'no'    => 1,
+        'size'  => 25,
         'total' => 1,
     ];
 
-    function get($f3) {
+    function get($f3)
+    {
         $track = new Mapper($this->db, 'oc_track');
         $tracks = $track->find([], $this->pageOptions());
 
+        $access = new Mapper($this->db, 'oc_track_access');
+        $register = new Mapper($this->db, 'oc_track_register');
+        $payment = new Mapper($this->db, 'oc_track_payment');
+        $statistics = [];
+
+        foreach ($tracks as $t) {
+            $filter = "code = '${t['code']}'";
+            $a = $access->findone($filter);
+            $statistics[$t['id']] = [];
+            $statistics[$t['id']]['access'] = $a ? $a['count'] : 0;
+            $statistics[$t['id']]['register'] = $register->count($filter);
+            $statistics[$t['id']]['payment'] = $payment->count($filter);
+        }
+
         $f3->set('tracks', $tracks);
+        $f3->set('statistics', $statistics);
         $f3->set('page', $this->page);
         $f3->set('title', 'Tracking code');
 
         echo \Template::instance()->render('track/index.html');
     }
 
-    function pageOptions() {
+    function pageOptions()
+    {
         //page no
-        if ($_GET['page'] && $_GET['page'] > 1) $this->page['no'] = $_GET['page'];
+        if ($_GET['page'] && $_GET['page'] > 1) {
+            $this->page['no'] = $_GET['page'];
+        }
 
         //page total
         $track = new Mapper($this->db, 'oc_track');
@@ -35,8 +55,8 @@ class Index extends AppBase {
         $offset = ($this->page['no'] - 1) * $this->page['size'];
 
         return [
-            'order' => 'id desc',
-            'limit' => $this->page['size'],
+            'order'  => 'id desc',
+            'limit'  => $this->page['size'],
             'offset' => $offset,
         ];
     }
